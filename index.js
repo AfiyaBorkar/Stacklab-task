fcd7f7654318c2ea903f52bfaafc5cc6a2d2a719
Afiya <afiyaborkar34@gmail.com>
2024-01-04 00:07:12 +0530

    made changes to index.js file

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ dbconnect();
 app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, './frontend/build/index.html'));
   });
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
 
 app.listen(port,()=>{
